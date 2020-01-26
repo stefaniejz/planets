07fa74eb414176905b515d21b8cb089c852ebc2e
stefaniejz <stephanie.jinzi.yan@gmail.com>
2020-01-25 22:24:55 -0800

    MecuryPage image and stats

--- src/MecuryComponent.js
+++ src/MecuryComponent.js
@@ -7,6 +7,24 @@ class MecuryComponent extends React.Component {
         return(
         <div className="Mecury">
             <MenuBar></MenuBar>
+            <div className="Mecury-page-img-container">
+              <img className="mecury-page-img" style={{width:"100%"}} 
+              src="https://mustlovecrows.com/wp-content/uploads/2019/10/free-intuitive-readings-must-love-crows.jpg">
+              </img>
+              <div className="mecury-page-img-text-title">
+                <p>Mecury
+                <br></br>
+                The Swiftest Planet
+                </p>
+              </div>
+              <div className="mecury-page-img-text-stats">
+                {/* <h4 style={{color:"white"}}>Lenth of year:</h4> */}
+                <p>Lenth of year: 88 earth days
+                    <br></br>
+                   Planet type: Terrestrial
+                </p>
+              </div>
+            </div>
         </div>
         
         )
