910c8c8e1599b2caab617562f90c22a6f6ba2a20
stefaniejz <stephanie.jinzi.yan@gmail.com>
2020-02-10 13:41:47 -0800

    Complete Mecury indepth page text and function

--- src/InDepth.js
+++ src/InDepth.js
@@ -3,14 +3,74 @@ import React from 'react'
 class InDepth extends React.Component{
     render() {
         return(
-            <div className="indepth">
+            <div className="indepth" >
                 <img src={this.props.detail.surface_image}></img>
-                <div className="indepth_description" >
+                <div className="indepth-description">
                     {this.props.detail.indepth_description.map((description, index)=>{
-                        return <div>
-                            <p style={{margin:"300"}}>{description}</p>
+                        return <div >
+                            <p>{description}</p>
                         </div>
                     })}
+                </div>
+                <h2>Size and Distance</h2>
+                <div className="size-and-distance">
+                    {this.props.detail.size_and_distance.map((element, index)=>{
+                        return <div>
+                            <p>{element}</p>
+                        </div>
+                    })}
+                </div>
+                <h2>Orbit and Rotation</h2>
+                <div className="orbit-and-rotation">
+                    {this.props.detail.orbit_and_rotation.map((element, index)=>{
+                        return <div>
+                            <p>{element}</p>
+                        </div>
+                    })}
+                </div>
+                <h2>Structure</h2>
+                <div className="indepth-structure">
+                    <p>{this.props.detail.structure}</p>
+                </div>
+                <h2>Formation</h2>
+                <div className="indepth-formation">
+                    {this.props.detail.formation.map((element, index)=>{
+                        return <div>
+                            <p>{element}</p>
+                        </div>
+                    })}
+                </div>
+                <h2>Kid Friendly</h2>
+                <div className="kid-friendly">
+                    {this.props.detail.kid_friendly.map((element, index)=>{
+                        return <div>
+                            <p>{element}</p>
+                        </div>
+                    })}
+                </div>
+                <h2>Surface</h2>
+                <div className="indepth-surface">
+                    {this.props.detail.surface.map((element, index)=>{
+                        return <div>
+                            <p>{element}</p>
+                        </div>
+                    })}
+                </div>
+                <h2>Atomosphere</h2>
+                <div className="indepth-atmosphere">
+                    <p>{this.props.detail.atmosphere}</p>
+                </div>
+                <h2>Magnetosphere</h2>
+                <div className="indepth-magnetosphere">
+                    <p>{this.props.detail.Megnetosphere}</p>
+                </div>
+                <h2>Moons</h2>
+                <div className="indepth-moons">
+                    <p>{this.props.detail.moons}</p>
+                </div>
+                <h2>Potential for Life</h2>
+                <div className="potential-for-life">
+                    <p>{this.props.detail.Potential_for_life}</p>
                 </div>
 
             </div>
