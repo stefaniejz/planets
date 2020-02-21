eccafeaad3533150f412fbbfcc9c91872dfda70e
stefaniejz <stephanie.jinzi.yan@gmail.com>
2020-02-21 13:14:12 -0800

    indepth page css

--- src/InDepth.js
+++ src/InDepth.js
@@ -3,7 +3,7 @@ import React from 'react'
 class InDepth extends React.Component{
     render() {
         return(
-            <div className="indepth">
+            <div className="indepth" style={{marginRight:350, marginLeft:350, marginBottom:50}}>
                 <img src={this.props.detail.surface_image}></img>
                 <div className="indepth-description">
                     {this.props.detail.indepth_description.map((description, index)=>{
@@ -12,7 +12,7 @@ class InDepth extends React.Component{
                         </div>
                     })}
                 </div>
-                <h2>Size and Distance</h2>
+                <h2 className="indepth-title">Size and Distance</h2>
                 <div className="size-and-distance">
                     {this.props.detail.size_and_distance.map((element, index)=>{
                         return <div>
@@ -20,7 +20,7 @@ class InDepth extends React.Component{
                         </div>
                     })}
                 </div>
-                <h2>Orbit and Rotation</h2>
+                <h2 className="indepth-title">Orbit and Rotation</h2>
                 <div className="orbit-and-rotation">
                     {this.props.detail.orbit_and_rotation.map((element, index)=>{
                         return <div>
@@ -28,7 +28,7 @@ class InDepth extends React.Component{
                         </div>
                     })}
                 </div>
-                <h2>Structure</h2>
+                <h2 className="indepth-title">Structure</h2>
                 <div className="indepth-structure">
                     {this.props.detail.structure.map((element, index)=>{
                             return <div>
@@ -36,7 +36,7 @@ class InDepth extends React.Component{
                             </div>
                         })}
                 </div>
-                <h2>Formation</h2>
+                <h2 className="indepth-title">Formation</h2>
                 <div className="indepth-formation">
                     {this.props.detail.formation.map((element, index)=>{
                         return <div>
@@ -44,7 +44,7 @@ class InDepth extends React.Component{
                         </div>
                     })}
                 </div>
-                <h2>Kid Friendly</h2>
+                <h2 className="indepth-title">Kid Friendly</h2>
                 <div className="kid-friendly">
                     {this.props.detail.kid_friendly.map((element, index)=>{
                         return <div>
@@ -52,7 +52,7 @@ class InDepth extends React.Component{
                         </div>
                     })}
                 </div>
-                <h2>Surface</h2>
+                <h2 className="indepth-title">Surface</h2>
                 <div className="indepth-surface">
                     {this.props.detail.surface.map((element, index)=>{
                         return <div>
@@ -60,11 +60,11 @@ class InDepth extends React.Component{
                         </div>
                     })}
                 </div>
-                <h2>Atomosphere</h2>
+                <h2 className="indepth-title">Atomosphere</h2>
                 <div className="indepth-atmosphere">
                     <p>{this.props.detail.atmosphere}</p>
                 </div>
-                <h2>Magnetosphere</h2>
+                <h2 className="indepth-title">Magnetosphere</h2>
                 <div className="indepth-magnetosphere">
                     {this.props.detail.Magnetosphere.map((element, index)=>{
                             return <div>
@@ -72,7 +72,7 @@ class InDepth extends React.Component{
                             </div>
                         })}
                 </div>
-                <h2>Moons</h2>
+                <h2 className="indepth-title">Moons</h2>
                 <div className="indepth-moons">
                     {this.props.detail.moons.map((element, index)=>{
                             return <div>
@@ -80,7 +80,7 @@ class InDepth extends React.Component{
                             </div>
                         })}
                 </div>
-                <h2>Potential for Life</h2>
+                <h2 className="indepth-title">Potential for Life</h2>
                 <div className="potential-for-life">
                     <p>{this.props.detail.Potential_for_life}</p>
                 </div>
