3b4db46bf8991ca011590d37c1502e2137aab47a
stefaniejz <stephanie.jinzi.yan@gmail.com>
2020-03-07 21:30:24 -0800

    exploration component css

--- src/Exploration.js
+++ src/Exploration.js
@@ -4,10 +4,17 @@ class Exploration extends React.Component {
     render() {
         return (
             <div className="exploration">
-                <div>
+                <div className="exploration-text">
                     {this.props.detail.exploration.map((para, index)=>{
                         return <div>
                             <p>{para}</p>
+                            </div>
+                    })}
+                </div>
+                <div className="exploration-events">
+                    {this.props.detail.significant_evets.map((event, index)=>{
+                        return <div>
+                            <p>{event}</p>
                             </div>
                     })}
                 </div>
