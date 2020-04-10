8a95c1eef711a2e18c3cc8ab0a6f8f39914cc2da
stefaniejz <stephanie.jinzi.yan@gmail.com>
2020-04-10 14:29:21 -0700

    marsmoons component indeprth and overview page

--- src/MarsMoonComponent.js
+++ src/MarsMoonComponent.js
@@ -9,8 +9,18 @@ class MarsMoonComponent extends React.Component {
         description1:"Asaph Hall was about to give up his frustrating search for a Martian moon one August night in 1877, but his wife Angelina urged him on. He discovered Deimos the next night, and Phobos six nights after that.",
         description2:"Ninety-four years later, NASA's Mariner 9 spacecraft got a much better look at the two moons from its orbit around Mars. The dominant feature on Phobos, it found, was a crater six miles (10 kilometers) wide—nearly half the width of the moon itself. It was given Angelina's maiden name: Stickney.",
         image:["https://solarsystem.nasa.gov/internal_resources/436",
-        "https://solarsystem.nasa.gov/internal_resources/435"]
-       
+        "https://solarsystem.nasa.gov/internal_resources/435"],
+        indepth_description:[],
+        size_and_distance:[],
+        orbit_and_rotation:[],
+        structure:[],
+        formation:[],
+        indepth_description:[],
+        kid_friendly:[],
+        surface:[],
+        Magnetosphere:[],
+        moons:[],
+        Potential_for_life:""
     }
 
     render(){
